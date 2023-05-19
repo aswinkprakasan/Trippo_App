8b8e3a70b7a265dcdd13af7921254ea76528a932
aswinkprakasan <aswinkuttanchery@gmail.com>
2023-05-19 13:59:49 +0530

    starting with signin and signup page

---
+++ app/src/main/java/com/example/trippoapp/SignupActivity.java
@@ -0,0 +1,67 @@
+package com.example.trippoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignupActivity extends AppCompatActivity {
+
+    EditText username, password, repassword;
+    Button btnSignup, btnSignin;
+    DBHelper myDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        repassword = findViewById(R.id.repassword);
+
+        btnSignup = findViewById(R.id.btnSignup);
+        btnSignin = findViewById(R.id.btnSignin);
+
+        myDB = new DBHelper(this);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
+
+                if(user.equals("") || pass.equals("") || repass.equals("")){
+                    Toast.makeText(SignupActivity.this, "Fill all the fields", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if (pass.equals(repass)){
+                        Boolean usercheckResult = myDB.checkuser(user);
+                        if (usercheckResult == false){
+                            Boolean regResult = myDB.insertData(user, pass);
+                            if (regResult == true){
+                                Toast.makeText(SignupActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), SigninActivity.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                Toast.makeText(SignupActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else {
+                            Toast.makeText(SignupActivity.this, "User Exists", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else {
+                        Toast.makeText(SignupActivity.this, "Password not matching", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+}
